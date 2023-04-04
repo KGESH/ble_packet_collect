74a94c3cc4d2088c26c44798eebf00312bdc74d6
KGESH <seonghyeon.jee@gmail.com>
2023-04-05 00:17:34 +0900

    [Feat] Config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { PrismaModule } from './database/prisma/prisma.module';
 import { RouterModule } from './router/router.module';
 import { SensorModule } from './sensor/sensor.module';
+import { ConfigsModule } from './configs/configs.module';
 
 @Module({
-  imports: [PrismaModule, RouterModule, SensorModule],
+  imports: [ConfigsModule, PrismaModule, RouterModule, SensorModule],
   controllers: [AppController],
 })
 export class AppModule {}
